c64a37416e287f8b49a5db1967a23726807cba4b
rachel <rachelkiffiak1@gmail.com>
2020-11-17 21:21:09 -0700

    Completed the request methods and added a list of animals.

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, Response
+import mariadb
+import dbcreds
+import json
+
+app = Flask(__name__)
+animals = ["Panda", "Koala bear", "Tiger", "Cheetah", "Grizzly Bear"]
+@app.route('/animals', methods =["GET", "POST", "PATCH", "DELETE"])
+def animals():
+    if request.method == "GET":
+      return Response(json.dumps(animals, default=str), mimetype="application/json", status=200)
+    elif request.method == "POST": 
+          return Response("Elk Inserted Successfully!", mimetype="text/html", status=201)
+    elif request.method == "PATCH":
+          return Response("Changed 'tiger' to 'Lion' successfully", mimetype="text/html", status=204)
+    elif request.method == "DELETE":
+          return Response("Deleted 'Grizzly bear' successfully ", mimetype="text/html", status=204)
+    else:
+          return Response("Error has occured ", mimetype="text/html", status=500)
